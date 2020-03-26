2be096f062bffefdf680de18c3fc4d2a1cca189d
xroigmartin <xroigmartin@gmail.com>
2020-03-26 18:09:04 +0100

    feat(build): Create project

---
+++ src/main/java/xroigmartin/gce/GceApplication.java
@@ -0,0 +1,13 @@
+package xroigmartin.gce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xroigmartin/gce/GceApplicationTests.java
@@ -0,0 +1,13 @@
+package xroigmartin.gce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
